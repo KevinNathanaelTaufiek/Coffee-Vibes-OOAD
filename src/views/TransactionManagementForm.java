2aaddcda42983ad3e48dc26164a37b399f380fac
KevinNathanael <kevin.taufiek@gmail.com>
2022-01-03 00:22:33 +0700

    confirm dialog checkout

--- src/views/TransactionManagementForm.java
+++ src/views/TransactionManagementForm.java
@@ -1,7 +1,9 @@
 package views;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
@@ -19,6 +21,7 @@ import controllers.TransactionHandler;
 import controllers.VoucherController;
 import models.CartItem;
 import models.Employee;
+import models.Transaction;
 
 public class TransactionManagementForm extends JFrame implements ActionListener{
 
@@ -72,8 +75,15 @@ public class TransactionManagementForm extends JFrame implements ActionListener{
 		String voucherID = voucherField.getText().toString();
 		
 		if(a.getSource() == checkoutButton) {
-			TransactionHandler.getInstance().insertTransaction(voucherID, String.valueOf(emp.getEmployeeID()), totalPrice);
-			JOptionPane.showMessageDialog(this, TransactionHandler.getInstance().getErrorMessage());
+			
+			int confrim = confirmationDialog("Check Out");
+			if(confrim == 1) {
+				TransactionHandler.getInstance().insertTransaction(voucherID, String.valueOf(emp.getEmployeeID()), totalPrice);
+				JOptionPane.showMessageDialog(this, TransactionHandler.getInstance().getErrorMessage());
+				
+			}else {
+				JOptionPane.showMessageDialog(this, "Check Out Canceled!");
+			}
 			this.dispose();
 		}
 		else if (a.getSource() == useVoucherButton) {
@@ -101,6 +111,31 @@ public class TransactionManagementForm extends JFrame implements ActionListener{
 		
 		this.totalPrice = totalPrice;
 		return totalPrice;
+	}
+	
+	public int confirmationDialog(String dialog) {
+		JFrame frame = new JFrame("Confirmation Dialog");
+	    JPanel panel = new JPanel();
+	    LayoutManager layout = new FlowLayout();  
+	    panel.setLayout(layout);       
+
+	    
+	    final JLabel label = new JLabel();
+	    int result = JOptionPane.showConfirmDialog(frame,"Are you sure you want "+dialog+" this item?", "Confirmation Dialog",
+	               JOptionPane.YES_NO_OPTION,
+	               JOptionPane.QUESTION_MESSAGE);
+	    if(result == JOptionPane.YES_OPTION){
+	    	return 1;
+	    }else if (result == JOptionPane.NO_OPTION){
+	    	return 0;
+	    }
+
+	    panel.add(label);
+	    frame.getContentPane().add(panel, BorderLayout.CENTER);    
+	    frame.setSize(560, 200);      
+	    frame.setLocationRelativeTo(null);  
+	    frame.setVisible(true);
+	    return 0;
 	}
 
 }
